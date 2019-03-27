f2eba983f350c0815d6efae6c4079fe70ada02a8
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-27 23:27:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,3 @@
 # {:blake => 500, :ashley => 2, :adam => 1}
 require "pry"
 
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil 
-  hash.each do |key, value|
-    if lowest_value == nil || value < lowest_value
-      lowest_value = value 
-      lowest_key = key 
-    end 
-  end 
-  lowest_key
-end 
